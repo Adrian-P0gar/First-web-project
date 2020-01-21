5e36ffc1669644ea5a36057268f4dc82f99e15c0
Alexandru Oriean <alexandru.oriean@gmail.com>
2020-01-21 12:10:24 +0200

    implemented sort_by function

--- data_handler.py
+++ data_handler.py
@@ -1,21 +1,29 @@
 import csv
+import time
 
 def read_csv():
     all_information = []
-    with open("/home/pogar/Web Module/TW I/ask-mate-python/sample_data/question.csv") as csvfile:
-        reader = csv.DictReader(csvfile)
+    with open("sample_data/question.csv") as csv_file:
+        reader = csv.DictReader(csv_file)
         for row in reader:
             all_information.append(dict(row))
     return all_information
 
 
+def sort_by(data, label='id', order='descending'):
+    for element in data:
+        element['id'] = int(element['id'])
+        element['submission_time'] = int(element['submission_time'])
+        element['view_number'] = int(element['view_number'])
+        element['vote_number'] = int(element['vote_number'])
+    if order == 'descending':
+        return [element for element in sorted(data, key=lambda x: x[label], reverse=True)]
+    return [element for element in sorted(data, key=lambda x: x[label])]
 
 
+def get_time_stamp():
+    return str(int(time.time()))
 
 
-
-
-
-
-
-
+def get_date_time(time_stamp):
+    return time.strftime("%a, %d %b %Y %H:%M:%S %Z", time.localtime(int(time_stamp)))
