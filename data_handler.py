0426d261ffcedfae14469d89d395b0a2a37c3e60
Adrian-P0gar <pogar.adi@gmail.com>
2020-01-21 16:27:24 +0200

    functional on this step.Add, edit question and home page

--- data_handler.py
+++ data_handler.py
@@ -1,16 +1,81 @@
-import csv
-import time
+import csv, sys, time
 
+DATA_HEADER = [ 'id','submission_time','view_number','vote_number','title','message','image']
+DATA_FILE_PATH = "/home/pogar/Web Module/TW I/ask-mate-python/sample_data/question.csv"
 def read_csv():
     all_information = []
-    with open("sample_data/question.csv") as csv_file:
-        reader = csv.DictReader(csv_file)
+    with open("/home/pogar/Web Module/TW I/ask-mate-python/sample_data/question.csv") as csvfile:
+        reader = csv.DictReader(csvfile)
         for row in reader:
             all_information.append(dict(row))
     return all_information
 
+def update_on_csv(story):
 
-def sort_by(data, label='id', order='descending'):
+    list_of_all = read_csv()
+    with open(DATA_FILE_PATH, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=DATA_HEADER)
+        writer.writeheader()
+
+        for row in list_of_all:
+            # if row['id'] != story['id']:
+            #     writer.writerow(row)
+            if row['id'] == story['id']:
+                row = story
+                # writer.writeheader()
+
+            writer.writerow(row)
+
+
+def get_csv_data(one_user_story_id=None):
+    """
+    :param one_user_story_id:
+        If given, it will act as a filter and return the dictionary of one specific User Story
+        If not given, it will return a list of dictionaries with all the details
+    :return:
+    """
+    #  create a temporary list to read each line
+    user_stories = []
+
+    #  open csv file to read
+    with open(DATA_FILE_PATH, encoding='utf-8') as csvfile:
+        #  use DictReader to directly create dictionaries from each lines in the csv file
+        reader = csv.DictReader(csvfile)
+
+        #  read all lines in csv file
+        for row in reader:
+            #  make a copy of the read row, since we can't modify it
+            user_story = dict(row)
+
+            # if filtered, then just return this _found_ user story
+            if one_user_story_id is not None and one_user_story_id == user_story['id']:
+                return user_story
+
+            #  store modified data in temporary list
+            user_stories.append(user_story)
+def next_id():
+    existing_data = read_csv()
+    if len(existing_data) == 0:
+        return 1
+    else:
+        id = len(existing_data) + 1
+        return  id
+
+def add_on_csv(story):
+    with open(DATA_FILE_PATH, 'a', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=DATA_HEADER)
+        # writer.writeheader()
+        story["id"] = next_id()
+        writer.writerow(story)
+
+def get_time_stamp():
+    return str(int(time.time()))
+
+def get_date_time(time_stamp):
+    return time.strftime("%a, %d %b %Y %H:%M:%S %Z", time.localtime(int(time_stamp)))
+
+
+def sort_by(data, label='submission_time', order='descending'):
     for element in data:
         element['id'] = int(element['id'])
         element['submission_time'] = int(element['submission_time'])
@@ -21,9 +86,9 @@ def sort_by(data, label='id', order='descending'):
     return [element for element in sorted(data, key=lambda x: x[label])]
 
 
-def get_time_stamp():
-    return str(int(time.time()))
 
 
-def get_date_time(time_stamp):
-    return time.strftime("%a, %d %b %Y %H:%M:%S %Z", time.localtime(int(time_stamp)))
+
+
+
+
--- server.py
+++ server.py
@@ -1,16 +1,57 @@
-from flask import Flask, render_template, request,redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 import data_handler as data
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/list')
+@app.route('/list', methods=['GET', 'POST'])
+
 def list():
-    information = data.read_csv()
-    return render_template('list.html', information=information , title="asdfghj")
+    default_information = data.read_csv()
+    if request.args.get('sort') == None:
+        information = data.sort_by(default_information)
+    else:
+        information = data.sort_by(default_information, request.args.get('sort'), request.args.get('sort_direction'))
+    return render_template('list.html', information=information , title="Home", data=data)
 
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    today_timer = data.get_time_stamp()
+    if request.method == "POST":
+        question_content = {
+            'submission_time': today_timer,
+            'title': request.form.get('title'),
+            'view_number': 0,
+            'vote_number': 0,
+            'message': request.form.get('message'),
+            'image': "?"
+        }
+        data.add_on_csv(dict(question_content))
+        return redirect('/list')
+    return render_template('add.html')
 
-
+@app.route('/edit/<question_id>', methods=['GET', 'POST'])
+def edit(question_id):
+    if request.method == 'POST':
+        form_id = request.form.get('form_id')
+        form_title = request.form.get('title')
+        if question_id == form_id:
+            question_content = {
+                'id': question_id,
+                'submission_time': request.form.get('submission_time'),
+                'title': request.form.get('title'),
+                'view_number': request.form.get('view_number'),
+                'vote_number': request.form.get('vote_number'),
+                'message': request.form.get('message'),
+                'image': request.form.get('image')
+            }
+            data.update_on_csv(question_content)
+            return redirect(url_for('list'))
+    question_content = data.get_csv_data(question_id)
+    return render_template('edit.html',
+                           question_content=question_content,
+                           question_id=question_id
+                           )
 if __name__ == '__main__':
     app.run(
         port= 5000,
